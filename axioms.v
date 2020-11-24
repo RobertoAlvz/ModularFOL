41da1f6e848dab2d093e43d3d9b0a4369372b631
Roberto Alvarez <roberto@artemis>
2020-11-24 16:42:51 +0100

    functoriality of vectors

--- axioms.v
+++ axioms.v
@@ -1,3 +1,12 @@
+(** * General Header Autosubst - Assumptions and Definitions *)
+
+(** ** Axiomatic Assumptions
+    For our development, during rewriting of the reduction rules,  we have to extend Coq with two well known axiomatic assumptions, namely _functional extensionality_ and _propositional extensionality_. The latter entails _proof irrelevance_.
+*)
+
+(** *** Functional Extensionality
+    We import the axiom from the Coq Standard Library and derive a utility tactic to make the assumption practically usable.
+*)
 Require Import Coq.Logic.FunctionalExtensionality.
 Require Import Program.Tactics.
 
@@ -15,54 +24,29 @@ Ltac fext := nointr repeat (
      refine (@forall_extensionalityS _ _ _ _)); intro
   end).
 
-(* ** Propositional Extensionality
-    We state the axiom of _propositional extensionality_ directly and use it to prove _proof irrelevance_.
-*)
-Axiom pext : forall P Q : Prop, (P <-> Q) -> (P = Q).
 
-Lemma pi {P : Prop} (p q : P) : p = q.
-Proof.
-  assert (P = True) by (apply pext; tauto). subst. now destruct p,q.
-Qed.
+(** ** Functor Instances
+Exemplary functor instances needed to make Autosubst's generation possible for functors.
+Two things are important:
+1. The names are fixed.
+2. For Coq to check termination, also the proofs have to be closed with Defined.
+ *)
 
-(* **** Functor Instances *)
-
-Definition funcomp {X Y Z} (g : Y -> Z) (f : X -> Y) :=
-  fun x => g (f x).
-
-(* **** Vector Instance *)
-Require Export Vector.
-
-Definition vec_ext {A B n} {f g : A -> B} :
-  (forall x, f x = g x) -> forall xs : Vector.t A n, Vector.map f xs = Vector.map g xs.
-  intros H. induction xs. reflexivity.
-  cbn. f_equal. apply H. apply IHxs.
-Defined.
-
-Definition vec_id {A n} { f : A -> A} :
-  (forall x, f x = x) -> forall xs : Vector.t A n, Vector.map f xs = xs.
-Proof.
-  intros H. induction xs. reflexivity.
-  cbn. rewrite H. rewrite IHxs; eauto.
-Defined.
-
-Definition vec_comp {A B C n} {f: A -> B} {g: B -> C} {h} :
-  (forall x, (funcomp g f) x = h x) -> forall xs : Vector.t A n, map g (map f xs) = map h xs.
-Proof.
-  induction xs. reflexivity.
-  cbn. rewrite <- H. f_equal. apply IHxs.
-Defined.
-
-(* **** List Instance *)
+(** *** List Instance *)
 Require Export List.
 
+Definition funcomp {X Y Z} (g : Y -> Z) (f : X -> Y)  :=
+  fun x => g (f x).
+
+Notation "'list_map'" := map.
+
 Definition list_ext {A B} {f g : A -> B} :
-  (forall x, f x = g x) -> forall xs, map f xs = map g xs.
+  (forall x, f x = g x) -> forall xs,  list_map f xs = map g xs.
   intros H. induction xs. reflexivity.
   cbn. f_equal. apply H. apply IHxs.
 Defined.
 
-Definition list_id {A} { f : A -> A} :
+Definition list_id {A}  { f : A -> A} :
   (forall x, f x = x) -> forall xs, List.map f xs = xs.
 Proof.
   intros H. induction xs. reflexivity.
@@ -70,13 +54,13 @@ Proof.
 Defined.
 
 Definition list_comp {A B C} {f: A -> B} {g: B -> C} {h} :
-  (forall x, (funcomp g f) x = h x) -> forall xs, map g (map f xs) = map h xs.
+  (forall x, (funcomp  g f) x = h x) -> forall xs, map g (map f xs) = map h xs.
 Proof.
   induction xs. reflexivity.
   cbn. rewrite <- H. f_equal. apply IHxs.
 Defined.
 
-(* **** Prod Instance *)
+(** *** Prod Instance *)
 
 Definition prod_map {A B C D} (f : A -> C) (g : B -> D) (p : A * B) :
   C * D.
@@ -97,21 +81,22 @@ Proof.
 Defined.
 
 Definition prod_comp {A B C D E F} {f1 : A -> C} {g1 : C -> E} { h1} {f2: B -> D} {g2: D -> F} {h2}:
-  (forall x, (funcomp g1 f1) x = h1 x) -> (forall x, (funcomp g2 f2) x = h2 x) -> forall p, prod_map g1 g2 (prod_map f1 f2 p) = prod_map h1 h2 p.
+  (forall x, (funcomp  g1 f1) x = h1 x) -> (forall x, (funcomp g2 f2) x = h2 x) -> forall p, prod_map g1 g2 (prod_map f1 f2 p) = prod_map h1 h2 p.
 Proof.
   intros. destruct p. cbn. f_equal; auto.
   now rewrite <- H. now rewrite <- H0.
 Defined.
 
-(* **** Function Instance *)
 
-Definition cod X A: Type := X -> A.
+(** *** Function Instance *)
+
+Definition cod X A:  Type :=  X -> A.
 
 Definition cod_map {X} {A B} (f : A -> B) (p : X -> A) :
   X -> B.
 Proof. eauto. Defined.
 
-(* Requires functional extensionality. *)
+(** Note that this requires functional extensionality. *)
 Definition cod_id {X} {A} {f : A -> A} :
   (forall x, f x = x) -> forall (p: X -> A), cod_map f p = p.
 Proof. intros H p. unfold cod_map. fext. congruence. Defined.
@@ -120,12 +105,28 @@ Definition cod_ext {X} {A B} {f f' : A -> B} :
   (forall x, f x = f' x) -> forall (p: X -> A), cod_map f p = cod_map f' p.
 Proof. intros H p. unfold cod_map. fext. congruence. Defined.
 
-Definition cod_ext' {X} {A B} {f f' : A -> B} (p : X -> A) :
-  (forall x, f (p x) = f' (p x)) -> cod_map f p = cod_map f' p.
-Proof. intros H. unfold cod_map. fext. congruence. Defined.
-
 Definition cod_comp {X} {A B C} {f : A -> B} {g : B -> C} {h} :
-  (forall x, (funcomp g f) x = h x) -> forall (p: X -> _), cod_map g (cod_map f p) = cod_map h p.
+  (forall x, (funcomp g f) x =  h x) -> forall (p: X -> _), cod_map g (cod_map f p) = cod_map h p.
 Proof. intros H p. unfold cod_map. fext. intros x. now rewrite <- H. Defined.
 
 Hint Rewrite in_map_iff : FunctorInstances.
+
+
+
+Require Import Coq.Vectors.Vector.
+
+Definition vect n t := Vector.t t n.
+
+Definition vect_map {A B} (f:A->B) {n} (v : vect n A) : vect n B := Vector.map f v.
+
+Definition vect_id {A} {f : A -> A} {n} :
+  (forall x, f x = x) -> forall (v : vect n A), vect_map f v = v.
+Proof. intros H v. unfold vect_map. rewrite <- Vector.map_id. now apply Vector.map_ext. Defined.
+
+Definition vect_ext {A B} {f f' : A -> B} {n} :
+  (forall x, f x = f' x) -> forall (v: vect n A), vect_map f v = vect_map f' v.
+Proof. intros H v. unfold vect_map. now apply Vector.map_ext. Defined.
+
+Definition vect_comp {A B C} {f : A -> B} {g : B -> C} {n} {h} :
+  (forall x, (funcomp g f) x =  h x) -> forall (v: vect n _), vect_map g (vect_map f v) = vect_map h v.
+Proof. intros H v. unfold vect_map. rewrite Vector.map_map. apply vect_ext. intro. now rewrite <- H. Defined.
