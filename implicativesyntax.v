1d973618641b97d23c3ef4e6e8897129f3bc18e1
Roberto Alvarez <roberto@artemis>
2020-12-06 18:20:48 +0100

    split syntax files

---
+++ implicativesyntax.v
@@ -0,0 +1,76 @@
+Require Export unscoped header_extensible.
+Require Export termsyntax.
+
+Section form_implicative.
+Variable form : Type.
+
+Variable subst_form : forall   (sigmaterm : ( fin ) -> term ) (s : form ), form .
+
+Variable idSubst_form : forall  (sigmaterm : ( fin ) -> term ) (Eqterm : forall x, sigmaterm x = (var_term ) x) (s : form ), subst_form sigmaterm s = s.
+
+Variable ext_form : forall   (sigmaterm : ( fin ) -> term ) (tauterm : ( fin ) -> term ) (Eqterm : forall x, sigmaterm x = tauterm x) (s : form ), subst_form sigmaterm s = subst_form tauterm s.
+
+Variable compSubstSubst_form : forall    (sigmaterm : ( fin ) -> term ) (tauterm : ( fin ) -> term ) (thetaterm : ( fin ) -> term ) (Eqterm : forall x, ((funcomp) (subst_term tauterm) sigmaterm) x = thetaterm x) (s : form ), subst_form tauterm (subst_form sigmaterm s) = subst_form thetaterm s.
+
+Inductive form_implicative  : Type :=
+  | Fal : form_implicative 
+  | Impl : ( form   ) -> ( form   ) -> form_implicative .
+
+Variable retract_form_implicative : retract form_implicative form.
+
+Definition Fal_  : _ :=
+  inj (Fal ).
+
+Definition Impl_  (s0 : form  ) (s1 : form  ) : _ :=
+  inj (Impl s0 s1).
+
+Lemma congr_Fal_  : Fal_  = Fal_  .
+Proof. congruence. Qed.
+
+Lemma congr_Impl_  { s0 : form   } { s1 : form   } { t0 : form   } { t1 : form   } (H1 : s0 = t0) (H2 : s1 = t1) : Impl_  s0 s1 = Impl_  t0 t1 .
+Proof. congruence. Qed.
+
+(* Variable retract_ren_form : forall   (xiterm : ( fin ) -> fin) s, ren_form xiterm (inj s) = ren_form_implicative xiterm s. *)
+
+Definition subst_form_implicative   (sigmaterm : ( fin ) -> term ) (s : form_implicative ) : form  :=
+    match s return form  with
+    | Fal   => Fal_ 
+    | Impl  s0 s1 => Impl_  ((subst_form sigmaterm) s0) ((subst_form sigmaterm) s1)
+    end.
+
+Variable retract_subst_form : forall   (sigmaterm : ( fin ) -> term ) s, subst_form sigmaterm (inj s) = subst_form_implicative sigmaterm s.
+
+Definition idSubst_form_implicative  (sigmaterm : ( fin ) -> term ) (Eqterm : forall x, sigmaterm x = (var_term ) x) (s : form_implicative ) : subst_form_implicative sigmaterm s = inj s :=
+    match s return subst_form_implicative sigmaterm s = inj s with
+    | Fal   => congr_Fal_ 
+    | Impl  s0 s1 => congr_Impl_ ((idSubst_form sigmaterm Eqterm) s0) ((idSubst_form sigmaterm Eqterm) s1)
+    end.
+
+Definition ext_form_implicative   (sigmaterm : ( fin ) -> term ) (tauterm : ( fin ) -> term ) (Eqterm : forall x, sigmaterm x = tauterm x) (s : form_implicative ) : subst_form_implicative sigmaterm s = subst_form_implicative tauterm s :=
+    match s return subst_form_implicative sigmaterm s = subst_form_implicative tauterm s with
+    | Fal   => congr_Fal_ 
+    | Impl  s0 s1 => congr_Impl_ ((ext_form sigmaterm tauterm Eqterm) s0) ((ext_form sigmaterm tauterm Eqterm) s1)
+    end.
+
+Definition compSubstSubst_form_implicative    (sigmaterm : ( fin ) -> term ) (tauterm : ( fin ) -> term ) (thetaterm : ( fin ) -> term ) (Eqterm : forall x, ((funcomp) (subst_term tauterm) sigmaterm) x = thetaterm x) (s : form_implicative ) : subst_form tauterm (subst_form_implicative sigmaterm s) = subst_form_implicative thetaterm s :=
+    match s return subst_form tauterm (subst_form_implicative sigmaterm s) = subst_form_implicative thetaterm s with
+    | Fal   => (eq_trans) (retract_subst_form (_) (Fal )) (congr_Fal_ )
+    | Impl  s0 s1 => (eq_trans) (retract_subst_form (_) (Impl (_) (_))) (congr_Impl_ ((compSubstSubst_form sigmaterm tauterm thetaterm Eqterm) s0) ((compSubstSubst_form sigmaterm tauterm thetaterm Eqterm) s1))
+    end.
+
+Lemma instId_form_implicative  : subst_form_implicative (var_term ) = inj .
+Proof. exact ((FunctionalExtensionality.functional_extensionality _ _ ) (fun x => idSubst_form_implicative (var_term ) (fun n => eq_refl) ((id) x))). Qed.
+
+Lemma compComp_form_implicative    (sigmaterm : ( fin ) -> term ) (tauterm : ( fin ) -> term ) (s : form_implicative ) : subst_form tauterm (subst_form_implicative sigmaterm s) = subst_form_implicative ((funcomp) (subst_term tauterm) sigmaterm) s .
+Proof. exact (compSubstSubst_form_implicative sigmaterm tauterm (_) (fun n => eq_refl) s). Qed.
+
+Lemma compComp'_form_implicative    (sigmaterm : ( fin ) -> term ) (tauterm : ( fin ) -> term ) : (funcomp) (subst_form tauterm) (subst_form_implicative sigmaterm) = subst_form_implicative ((funcomp) (subst_term tauterm) sigmaterm) .
+Proof. exact ((FunctionalExtensionality.functional_extensionality _ _ ) (fun n => compComp_form_implicative sigmaterm tauterm n)). Qed.
+
+Definition isIn_form_form_implicative (s : form) (t : form_implicative) : Prop :=
+  match t with
+  | Fal   => False
+  | Impl t0 t1  => or (s = t0) (s = t1)
+  end.
+
+End form_implicative.
---
+++ termsyntax.v
@@ -0,0 +1,72 @@
+Require Export unscoped header_extensible.
+
+Class Signature := B_S { Funcs : Type; fun_ar : Funcs -> nat ; Preds : Type; pred_ar : Preds -> nat }.
+Context {Sigma : Signature}.
+
+Section term.
+Inductive term  : Type :=
+  | var_term : ( fin ) -> term 
+  | Func : forall (f : Funcs), ( vect (fun_ar f) (term  ) ) -> term .
+
+Lemma congr_Func { f : Funcs }  { s0 : vect (fun_ar f) (term  ) } { t0 : vect (fun_ar f) (term  ) } (H1 : s0 = t0) : Func  f s0 = Func  f t0 .
+Proof. congruence. Qed.
+
+Fixpoint subst_term   (sigmaterm : ( fin ) -> term ) (s : term ) : term  :=
+    match s return term  with
+    | var_term  s => sigmaterm s
+    | Func  f s0 => Func  f ((vect_map (subst_term sigmaterm)) s0)
+    end.
+
+Definition up_term_term   (sigma : ( fin ) -> term ) : ( fin ) -> term  :=
+  (scons) ((var_term ) (var_zero)) ((funcomp) (subst_term ((funcomp) (var_term ) (shift))) sigma).
+(* 
+Definition upId_term_term  (sigma : ( fin ) -> term ) (Eq : forall x, sigma x = (var_term ) x) : forall x, (up_term_term sigma) x = (var_term ) x := 
+  fun n => match n with
+  | S fin_n => (ap) (subst_term ((funcomp) (var_term ) (shift))) (Eq fin_n)
+  | 0  => eq_refl
+  end.
+
+Fixpoint idSubst_term  (sigmaterm : ( fin ) -> term ) (Eqterm : forall x, sigmaterm x = (var_term ) x) (s : term ) : subst_term sigmaterm s = s :=
+    match s return subst_term sigmaterm s = s with
+    | var_term  s => Eqterm s
+    | Func  f s0 => congr_Func ((vect_id (idSubst_term sigmaterm Eqterm)) s0)
+    end.
+
+Definition upExt_term_term   (sigma : ( fin ) -> term ) (tau : ( fin ) -> term ) (Eq : forall x, sigma x = tau x) : forall x, (up_term_term sigma) x = (up_term_term tau) x :=
+  fun n => match n with
+  | S fin_n => (ap) (subst_term ((funcomp) (var_term ) (shift))) (Eq fin_n)
+  | 0  => eq_refl
+  end.
+
+Fixpoint ext_term   (sigmaterm : ( fin ) -> term ) (tauterm : ( fin ) -> term ) (Eqterm : forall x, sigmaterm x = tauterm x) (s : term ) : subst_term sigmaterm s = subst_term tauterm s :=
+    match s return subst_term sigmaterm s = subst_term tauterm s with
+    | var_term  s => Eqterm s
+    | Func  f s0 => congr_Func ((vect_ext (ext_term sigmaterm tauterm Eqterm)) s0)
+    end.
+
+Fixpoint compSubstSubst_term    (sigmaterm : ( fin ) -> term ) (tauterm : ( fin ) -> term ) (thetaterm : ( fin ) -> term ) (Eqterm : forall x, ((funcomp) (subst_term tauterm) sigmaterm) x = thetaterm x) (s : term ) : subst_term tauterm (subst_term sigmaterm s) = subst_term thetaterm s :=
+    match s return subst_term tauterm (subst_term sigmaterm s) = subst_term thetaterm s with
+    | var_term  s => Eqterm s
+    | Func  f s0 => congr_Func ((vect_comp (compSubstSubst_term sigmaterm tauterm thetaterm Eqterm)) s0)
+    end.
+
+Definition up_subst_subst_term_term    (sigma : ( fin ) -> term ) (tauterm : ( fin ) -> term ) (theta : ( fin ) -> term ) (Eq : forall x, ((funcomp) (subst_term tauterm) sigma) x = theta x) : forall x, ((funcomp) (subst_term (up_term_term tauterm)) (up_term_term sigma)) x = (up_term_term theta) x :=
+  fun n => match n with
+  | S fin_n => (eq_trans) (compSubstSubst_term ((funcomp) (var_term ) (shift)) (up_term_term tauterm) ((funcomp) (up_term_term tauterm) (shift)) (fun x => eq_refl) (sigma fin_n)) ((eq_trans) ((eq_sym) (compSubstSubst_term tauterm ((funcomp) (var_term ) (shift)) ((funcomp) (subst_term ((funcomp) (var_term ) (shift))) tauterm) (fun x => eq_refl) (sigma fin_n))) ((ap) (subst_term ((funcomp) (var_term ) (shift))) (Eq fin_n)))
+  | 0  => eq_refl
+  end.
+
+Lemma instId_term  : subst_term (var_term ) = id .
+Proof. exact ((FunctionalExtensionality.functional_extensionality _ _ ) (fun x => idSubst_term (var_term ) (fun n => eq_refl) ((id) x))). Qed.
+
+Lemma varL_term   (sigmaterm : ( fin ) -> term ) : (funcomp) (subst_term sigmaterm) (var_term ) = sigmaterm .
+Proof. exact ((FunctionalExtensionality.functional_extensionality _ _ ) (fun x => eq_refl)). Qed.
+
+Lemma compComp_term    (sigmaterm : ( fin ) -> term ) (tauterm : ( fin ) -> term ) (s : term ) : subst_term tauterm (subst_term sigmaterm s) = subst_term ((funcomp) (subst_term tauterm) sigmaterm) s .
+Proof. exact (compSubstSubst_term sigmaterm tauterm (_) (fun n => eq_refl) s). Qed.
+
+Lemma compComp'_term    (sigmaterm : ( fin ) -> term ) (tauterm : ( fin ) -> term ) : (funcomp) (subst_term tauterm) (subst_term sigmaterm) = subst_term ((funcomp) (subst_term tauterm) sigmaterm) .
+Proof. exact ((FunctionalExtensionality.functional_extensionality _ _ ) (fun n => compComp_term sigmaterm tauterm n)). Qed.
+*)
+
+End term.
